7fe0d07cc08620716ac5428031ec9135745392ca
mgeojames <mgeojames@gmail.com>
2022-12-04 13:37:27 +0100

    advent day 3, questions 3a and 3b

--- ad3.py
+++ ad3.py
@@ -1,7 +1,9 @@
 X = [l.strip()for l in open('input3.txt')]
 
-common = []
-score = 0
+#part a
+common = [] #stores the list of common elements
+score = 0 #total score
+
 for x in X:
     firstHalf = x[:len(x)//2]
     secondHalf = x[len(x)//2:]
@@ -10,9 +12,35 @@ for x in X:
         if item in secondHalf:
             common.append(item)
             if ('a' <= item <= 'z'):
+                score += ord(item)-96 #ord('a')=97, so -96 will give score as 1 for 'a'
+            else:
+                score += ord(item)-38 #this will give score as 27 for 'A'
+            break
+
+print('part 1 score: ', score)
+
+##############################################
+#part b - method 1
+
+Y = [l.strip() for l in open('input3.txt')]
+group = []
+common = []
+score = 0
+
+for i in range(0, len(Y), 3):
+    group.append(Y[i])
+    group.append(Y[i+1])
+    group.append(Y[i+2])
+
+    for item in group[0]:
+        if item in group[1] and item in group[2]:
+            common.append(item)
+            if ('a' <= item <= 'z'):
                 score += ord(item)-96
             else:
                 score += ord(item)-38
             break
+    
+    group = []
 
-print('score: ', score)
+print('part 2 score: ', score)
