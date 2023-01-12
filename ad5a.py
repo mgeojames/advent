01c30204e6275c3cc7ce01b9e5094231282ba79a
mgeojames <mgeojames@gmail.com>
2023-01-12 23:48:00 +0100

    day 05 a & b

---
+++ ad5a.py
@@ -0,0 +1,34 @@
+S1 = ['B','L','D','T','W','C','F','M']
+S2 = ['N','B','L']
+S3 = ['J','C','H','T','L','V']
+S4 = ['S','P','J','W']
+S5 = ['Z','S','C','F','T','L','R']
+S6 = ['W','D','G','B','H','N','Z']
+S7 = ['F','M','S','P','V','G','C','N']
+S8 = ['W','Q','R','J','F','V','C','Z']
+S9 = ['R','P','M','L','H']
+stack = [[],S1, S2, S3, S4, S5, S6, S7, S8, S9]
+ 
+
+with open ('input5.txt') as fin:
+    input_op = fin.readlines()[10:]
+
+print('len(input): ', len(input_op))
+
+    
+for line in input_op:
+    split = line.split(' ')
+    count, start, stop = map(int, [split[1], split[3], split[5]])
+    # print(count, start, stop)
+
+    for i in range(count):
+        stack[stop].append(stack[start].pop())        
+    
+message = []
+stack.pop(0)
+for element in stack:
+    message.append(element[-1])
+    # print(element)
+
+print('message:', ''.join(message))
+    
---
+++ ad5b.py
@@ -0,0 +1,40 @@
+S1 = ['B','L','D','T','W','C','F','M']
+S2 = ['N','B','L']
+S3 = ['J','C','H','T','L','V']
+S4 = ['S','P','J','W']
+S5 = ['Z','S','C','F','T','L','R']
+S6 = ['W','D','G','B','H','N','Z']
+S7 = ['F','M','S','P','V','G','C','N']
+S8 = ['W','Q','R','J','F','V','C','Z']
+S9 = ['R','P','M','L','H']
+stack = [[],S1, S2, S3, S4, S5, S6, S7, S8, S9]
+ 
+
+with open ('input5.txt') as fin:
+    input_op = fin.readlines()[10:]
+
+print('len(input): ', len(input_op))
+
+for line in input_op:
+    split = line.split(' ')
+    count, start, stop = map(int, [split[1], split[3], split[5]])
+    # print(count, start, stop)
+
+    # print('before: \n from - ', stack[start], '\nto - ', stack[stop])
+    popped = []
+    for i in range(count):
+        popped.append(stack[start].pop())
+    # print('after popping:', popped)
+    popped = [popped[len(popped)-1-i] for i in range(len(popped))]
+    # print('after reversal:', popped)
+    
+    [stack[stop].append(popped[i]) for i in range (len(popped))]
+    # print('after appendinf:', stack[stop])
+
+message = []
+stack.pop(0)
+for element in stack:
+    message.append(element[-1])
+    # print(element)
+
+print('message:', ''.join(message))
