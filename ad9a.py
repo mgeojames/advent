9c1c369c75a8958d69697172d37101a28b68dc54
mgeojames <mgeojames@gmail.com>
2023-01-15 18:17:55 +0100

    day 09.a

--- ad9a.py
+++ ad9a.py
@@ -1,49 +1,76 @@
-with open ('sample.txt') as fin:
-    input = fin.read().split('\n')
+with open('input9.txt') as f:
+    X = f.read().split('\n')
 
-hx, hy = [1, 1]
-tx, ty = [2, 0]
+hx, hy = [0, 0]
+tx, ty = [0, 0]
 contact = False
-tailVisited = []
-tailVisited.append([tx, ty])
+tailvisit = []
+tailvisit.append([tx, ty])
 
-def printall():
-    print('hx:', hx, 'hy:', hy, '\ntx:', tx, 'ty:', ty)
-
-for line in input:
-    print('on start')
-    printall()
-
-    direction, steps = line.split(' ')
-    print('dir:', direction, ' steps:', steps)
-
-    if direction == 'R':
+for line in X:
+    dir, steps = line.split(' ')
+    if dir == 'R':
         for _ in range(int(steps)):
-            # move head in right direction
+            # move  head in right direction
             hy += 1
-            print('new head pos after R')
-            printall()
-        
-            # check if tail is in contact with head after head move
-            if (abs(hx-tx) <= 1 and abs(hy-ty)<=1):
-                contact = True # => no movement of tail
-                print('in contact')
-            else: # => tail movement, row wise, col wise, diagonally
-                print('no contact, tail movement')
-                if (hx == tx):            
+            # check if tail inin contact with head
+            if (abs(hx - tx) <= 1 and abs(hy-ty) <= 1):
+                contact = True  # No tail movement
+            else:  # tail movement(row,column or diagonal)
+                if (hx == tx):
                     changeX = 0
                     changeY = 1
                 else:
-                    changeX = int((hx-tx)/abs(hx-tx))
-                    changeY = int((hy-ty)/abs(hy-ty))
-                    print(changeX, changeY)
-                
+                    changeX = (hx-tx)/abs(hx-tx)
+                    changeY = (hy-ty)/abs(hy-ty)
                 tx += changeX
                 ty += changeY
-                tailVisited.append([tx, ty])
-    
-            print('updated values')
-            printall()
+                tailvisit.append([tx, ty])
+    if dir == 'L':
+        for _ in range(int(steps)):
+            hy += -1  # move  head in left direction
+            if (abs(hx - tx) <= 1 and abs(hy-ty) <= 1):  # check if tail inin contact with head
+                contact = True  # No tail movement
+            else:  # tail movement(row,column or diagonal)
+                if (hx == tx):
+                    changeX = 0
+                    changeY = -1
+                else:
+                    changeX = (hx-tx)/abs(hx-tx)
+                    changeY = (hy-ty)/abs(hy-ty)
+                tx += changeX
+                ty += changeY
+                tailvisit.append([tx, ty])
+    if dir == 'U':
+        for _ in range(int(steps)):
+            hx += -1  # move  head in upper direction
+            if (abs(hx - tx) <= 1 and abs(hy-ty) <= 1):  # check if tail inin contact with head
+                contact = True  # No tail movement
+            else:  # tail movement(row,column or diagonal)
+                if (hy == ty):
+                    changeX = -1
+                    changeY = 0
+                else:
+                    changeX = (hx-tx)/abs(hx-tx)
+                    changeY = (hy-ty)/abs(hy-ty)
+                tx += changeX
+                ty += changeY
+                tailvisit.append([tx, ty])
+    if dir == 'D':
+        for _ in range(int(steps)):
+            hx += 1  # move  head in down direction
+            if (abs(hx - tx) <= 1 and abs(hy-ty) <= 1):  # check if tail inin contact with head
+                contact = True  # No tail movement
+            else:  # tail movement(row,column or diagonal)
+                if (hy == ty):
+                    changeX = 1
+                    changeY = 0
+                else:
+                    changeX = (hx-tx)/abs(hx-tx)
+                    changeY = (hy-ty)/abs(hy-ty)
+                tx += changeX
+                ty += changeY
+                tailvisit.append([tx, ty])
 
-print('tails visited: ', len(tailVisited))
-
+my_set = set(tuple(x) for x in tailvisit)
+print("No.of sites tail visited atleast once : ", len(my_set))
